11bce21b8fa53eb4909c216077ab62e273c1d447
JorgeLIV <23170082@uttcampus.edu.mx>
2024-11-09 17:42:57 -0600

    Tabla Roles y Usuarios vinculadas

--- database/migrations/2024_11_09_232452_roles.php
+++ database/migrations/2024_11_09_232452_roles.php
@@ -13,18 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->unsignedBigInteger('role_id')->default(1); 
-            $table->rememberToken();
             $table->timestamps();
-
-            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
         });
+        //
     }
 
     /**
@@ -34,6 +28,7 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('roles');
+        //
     }
 };
